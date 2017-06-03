e0429c733cbd8453b7d30dde910f97a89e8705c2
acortds <acortds@users.noreply.github.com>
2017-06-03 14:25:03 -0500

    init file

---
+++ nooa_storm_data_analysis.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+
+getfile <- function(source_file, dest_file, file_type) {
+      pwd <- getwd()
+      
+      if (dir.exists("workdir")) {
+            unlink("workdir", recursive = TRUE)
+      }
+      
+      dir.create("workdir")
+      setwd("workdir")
+      
+      fileURL <- source_file
+      download.file(fileURL, destfile = dest_file,method = "curl")
+      
+      if (file_type = "zip") {
+            unzip(dest_file)
+      }
+      
+      setwd(pwd)
+}
