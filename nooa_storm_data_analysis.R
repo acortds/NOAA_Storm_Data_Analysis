44f5c6b519c86c830418993c309e9530c5966378
acortds <acortds@users.noreply.github.com>
2017-06-03 14:39:44 -0500

    getfile function complete

--- nooa_storm_data_analysis.R
+++ nooa_storm_data_analysis.R
@@ -13,9 +13,12 @@ getfile <- function(source_file, dest_file, file_type) {
       fileURL <- source_file
       download.file(fileURL, destfile = dest_file,method = "curl")
       
-      if (file_type = "zip") {
+      if (file_type == "zip") {
             unzip(dest_file)
+      } else if (file_type == "bz2") {
+            system(paste("/usr/bin/bunzip2 ", dest_file))
       }
       
       setwd(pwd)
 }
+
