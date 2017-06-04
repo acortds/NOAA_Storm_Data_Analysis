cda73b22a55e300fa2cc05247fb1494713e84d60
acortds <acortds@users.noreply.github.com>
2017-06-04 09:15:21 -0500

    changes to health_data_gt0 function

--- noaa_storm_data_analysis.R
+++ noaa_storm_data_analysis.R
@@ -38,17 +38,16 @@ get_amount <- function(quantity, expression){
       }
 }
 
-simplify_amt <- function(quantity){
-      B<-1000000000
-      M<-1000000
+simplify_amt <- function(quantity, mny_unit = "M", precision = 3){
       K<-1000
-      
-      if (quantity < M){
-            return(paste0(round(quantity/K,digits = 3),"K"))
-      } else if (quantity >= M & quantity < B){
-            return(paste0(round(quantity/M,digits = 3),"M"))
-      } else if (quantity >= B){
-            return(paste0(round(quantity/B,digits = 3),"B"))
+      M<-1000000
+      B<-1000000000
+      if (mny_unit == "K") {
+            return(paste0(round(quantity/K,digits = precision),"K"))
+      } else if (mny_unit == "M") {
+            return(paste0(round(quantity/M,digits = precision),"M"))
+      } else if (mny_unit == "B") {
+            return(paste0(round(quantity/B,digits = precision),"B"))
       }
 }
 
@@ -69,10 +68,25 @@ library(ggplot2)
 
 # Across the United States, which types of events (as indicated in the EVTYPE 
 # variable) are most harmful with respect to population health?
-storm_data_gt0 <- storm_data %>%
+health_data_gt0 <- storm_data %>%
       select (EVTYPE, 
               FATALITIES, 
-              INJURIES,
+              INJURIES) %>%
+      mutate(event_type=EVTYPE) %>%
+      group_by(event_type) %>%
+      summarise(fatalities = sum(FATALITIES), 
+                injuries = sum(INJURIES), 
+                total_event=fatalities+injuries,
+                pct_fatalities = round(fatalities/total_event,2),
+                pct_injuries = round(injuries/total_event,2)) %>%
+      filter(total_event > 0) %>%
+      arrange(desc(pct_fatalities)) %>%
+      top_n(20,fatalities )
+
+# Across the United States, which types of events have the greatest economic 
+# consequences?
+econo_data_gt0 <- storm_data %>%
+      select (EVTYPE, 
               PROPDMG,
               PROPDMGEXP,
               CROPDMG,
@@ -80,20 +94,14 @@ storm_data_gt0 <- storm_data %>%
               WFO) %>%
       mutate(event_type=EVTYPE) %>%
       group_by(event_type) %>%
-      mutate (propdmgdll = get_amount(PROPDMG,PROPDMGEXP), 
+      mutate (propdmgdll = get_amount(PROPDMG, PROPDMGEXP), 
               cropdmgdll = get_amount(CROPDMG, CROPDMGEXP)) %>%
-      summarise(fatalities = sum(FATALITIES), 
-                injuries = sum(INJURIES), 
-                total_event=fatalities+injuries,
-                total_prop_dmg = sum(propdmgdll),
+      summarise(total_prop_dmg = sum(propdmgdll),
                 total_crop_dmg = sum(cropdmgdll),
-                total_dmg = simplify_amt(total_prop_dmg+total_crop_dmg)) %>%
-      mutate (total_prop_dmg=simplify_amt(total_prop_dmg),
-              total_crop_dmg=simplify_amt(total_crop_dmg)) %>%
-      filter(total_event > 0) %>%
-      arrange(desc(total_event), 
-              desc(fatalities))
-
-# Across the United States, which types of events have the greatest economic 
-# consequences?
-
+                total_dmg = total_prop_dmg+total_crop_dmg) %>%
+      mutate (total_prop_dmg_m=simplify_amt(total_prop_dmg),
+              total_crop_dmg_m=simplify_amt(total_crop_dmg),
+              total_dmg_m=simplify_amt(total_dmg)) %>%
+      filter(total_dmg > 0) %>%
+      arrange(desc(total_dmg)) %>%
+      top_n(20)
